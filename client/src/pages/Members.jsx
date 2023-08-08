a1503ada3038c4d1b0adbd8dee6a662e27938d87
Prasoon <prasoon1308@gmail.com>
2023-08-08 15:23:48 +0530

    Members page

--- client/src/pages/Members.jsx
+++ client/src/pages/Members.jsx
@@ -1,4 +1,8 @@
-const Members = () => {}
+const Members = () => {
+    return <>
+    <p>Gm! Thank you for being a holder of the NFT collection, Appreciate your trust on ours!</p>
+    </>
+}
 
 
 export default Members;
