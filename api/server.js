f457dc020bb7e0d96a835679541f3aea61d99aa9
Prasoon <prasoon1308@gmail.com>
2023-08-08 15:23:19 +0530

    server issue resolved

--- api/server.js
+++ api/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const { Web3 } = require("web3");
 const ABI = require("./ABI.json");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 
-const contractAddress = process.env.CONTRACT_ADDRESS
-const quicknodeUrl = process.env.QUICKNODE_URL
-const account1 = process.env.ACCOUNT1
-const account2 = process.env.ACCOUNT2
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const quicknodeUrl = process.env.QUICKNODE_URL;
+const account1 = process.env.ACCOUNT1;
+const account2 = process.env.ACCOUNT2;
 
 const app = express();
 app.use(cors());
@@ -17,31 +17,38 @@ const web3 = new Web3(quicknodeUrl);
 const contract = new web3.eth.Contract(ABI, contractAddress);
 console.log(contract);
 
-const fetchNFTs = async () => {
+const fetchNFTs = async (account) => {
   try {
-    const nftBalance = await contract.methods.balanceOf(account1).call();
-    return {userNFTs : Number(nftBalance)}
+    const nftBalance = await contract.methods.balanceOf(account).call();
+    return { userNFTs: Number(nftBalance) };
   } catch (error) {
-    console.log("Error in fetching the NFTs: ",error)
+    console.log("Error fetching NFTs", error);
   }
-}
+};
 
-app.post("/members", async(req, res)=>{
-    try {
-        const account = req.body.from
-        const numNFTs = await fetchNFTs(account)
+app.post("/members", async (req, res) => {
+  try {
+    const account = req.body.from;
+    console.log(account);
+    const numNFTs = await fetchNFTs(account);
 
-        if(numNFTs.userNFTs > 0){
-            res.status(200).json({status : 200, numNFTs})
-        }else{
-            res.status(400).json({status : 400, message : "You don't have any NFT!"})
-        }
-    } catch (error) {
-        res.status(500).json({error: "Internal Server Error"})
+    if (numNFTs.userNFTs > 0) {
+      res.status(200).json({ status: 200, numNFTs });
+    } else {
+      res
+        .status(404)
+        .json({ status: 404, message: "You don't own any nft", numNFTs });
     }
+  } catch (error) {
+    res.status(500).json({ status: 500, message: "Internal Server Error" });
+  }
+});
+
+app.post("/webhook", async(req, res)=>{
+  
 })
 
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log("Server is running at ${PORT}");
+const server = app.listen(PORT, () => {
+  console.log(`Sever running at ${PORT}`);
 });
